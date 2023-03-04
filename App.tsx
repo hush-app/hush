3930a958a2c2fd6ef6cbeb980e420cd5f10baccc
Kwaku Amofah-Boafo <boafo819@gmail.com>
2023-03-04 01:21:10 -0500

    Adding Nativewind (aka TailwindCSS) and React Navigation to project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
+/// <reference types="nativewind/types"/>
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className='className="flex-1 items-center justify-center bg-white"'>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+    
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
